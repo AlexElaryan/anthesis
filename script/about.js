3bb3cbeb5a37b44dc50edf82bcce82285cd1beb3
AlexElaryan <alexelaryan1@gmail.com>
2025-03-20 18:24:09 +0400

    dep

---
+++ script/about.js
@@ -0,0 +1,3 @@
+const aboutintroBg = document.querySelector('.about-intro > img');
+
+window.addEventListener('DOMContentLoaded', pictureChanger(aboutintroBg, 'image/aboutintro.png', 'image/aboutintrolap.png', 'image/aboutintromob.png'));
